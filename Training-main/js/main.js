1d27db5e396f93a2972725bb7d402a620c17b4f3
VinceCarloCruz <122804206+shishiro170@users.noreply.github.com>
2023-02-18 10:05:52 +0800

    Add files via upload

---
+++ Training-main/js/main.js
@@ -0,0 +1,16 @@
+
+window.onscroll= function() {scrollFunction()};
+
+function scrollFunction(){
+    let mybutton= document.getElementById("myBtn");
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        mybutton.style.display ="block";
+    } else{
+        mybutton.style.display="none";
+    }
+}
+function topFunction (){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
